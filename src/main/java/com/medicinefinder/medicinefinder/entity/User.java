6d65bd53c9b6808e9f10a7a3beedbc7dc5213a2b
hirunidasunika <dasunikahiru9@gmail.com>
2023-02-23 11:38:35 +0530

    Add customer attributes to User entity class

--- src/main/java/com/medicinefinder/medicinefinder/entity/User.java
+++ src/main/java/com/medicinefinder/medicinefinder/entity/User.java
@@ -1,4 +1,32 @@
 package com.medicinefinder.medicinefinder.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "hd_users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "address")
+    private String address;
+
 }
