e072577e0382021e9ff7f8a8e7a5c3847725b95d
hirunidasunika <dasunikahiru9@gmail.com>
2023-02-23 11:50:16 +0530

    create Drug repository interface

--- src/main/java/com/medicinefinder/medicinefinder/repository/DrugRepository.java
+++ src/main/java/com/medicinefinder/medicinefinder/repository/DrugRepository.java
@@ -1,2 +1,9 @@
-package com.medicinefinder.medicinefinder.repository;public class DrugRepository {
+package com.medicinefinder.medicinefinder.repository;
+
+import com.medicinefinder.medicinefinder.entity.Drug;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DrugRepository extends JpaRepository<Drug,Integer> {
 }
