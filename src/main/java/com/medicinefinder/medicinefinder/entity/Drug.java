078bcedaabeb4f1d42cb1b6d768788a606f1a216
hirunidasunika <dasunikahiru9@gmail.com>
2023-02-23 11:46:36 +0530

    Create Drug entity class

--- src/main/java/com/medicinefinder/medicinefinder/entity/Drug.java
+++ src/main/java/com/medicinefinder/medicinefinder/entity/Drug.java
@@ -1,2 +1,32 @@
-package com.medicinefinder.medicinefinder.entity;public class Drug {
+package com.medicinefinder.medicinefinder.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "hd_drugs")
+public class Drug {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "drug_name")
+    private String drugName;
+
+    @Column(name = "drug_brand")
+    private String drugBrand;
+
+    @Column(name = "drug_weight")
+    private double drugWeight;
+
+    @Column(name = "stock_available")
+    private boolean isStockAvailable;
+
 }
