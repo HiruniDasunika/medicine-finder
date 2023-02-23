5dcc6041a115bc8e4776a93f357e4afc03f7b703
hirunidasunika <dasunikahiru9@gmail.com>
2023-02-23 12:03:42 +0530

    Create DrugServiceImpl class

--- src/main/java/com/medicinefinder/medicinefinder/service/DrugService.java
+++ src/main/java/com/medicinefinder/medicinefinder/service/DrugService.java
@@ -1,2 +1,15 @@
-package com.medicinefinder.medicinefinder.service;public class DrugService {
+package com.medicinefinder.medicinefinder.service;
+
+import com.medicinefinder.medicinefinder.entity.Drug;
+
+
+
+public interface DrugService {
+
+    Drug saveDrug(Drug drug);
+
+    Drug getDrugById(int id);
+
+
+
 }
--- src/main/java/com/medicinefinder/medicinefinder/service/serviceImplementation/DrugServiceImpl.java
+++ src/main/java/com/medicinefinder/medicinefinder/service/serviceImplementation/DrugServiceImpl.java
@@ -1,2 +1,23 @@
-package com.medicinefinder.medicinefinder.service.serviceImplementation;public class DrugServiceImpl {
+package com.medicinefinder.medicinefinder.service.serviceImplementation;
+
+import com.medicinefinder.medicinefinder.entity.Drug;
+import com.medicinefinder.medicinefinder.repository.DrugRepository;
+import com.medicinefinder.medicinefinder.service.DrugService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service(value = "drugService")
+public class DrugServiceImpl implements DrugService {
+
+    @Autowired
+    private DrugRepository repository;
+    @Override
+    public Drug saveDrug(Drug drug) {
+        return repository.save(drug);
+    }
+
+    @Override
+    public Drug getDrugById(int id) {
+        return repository.findById(id).orElse(null);
+    }
 }
